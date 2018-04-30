d6759e1917688ecab0221357e0bbaa0650a86c80
Brett Higgins <brett.higgins@gmail.com>
2018-04-30 09:40:30 -0400

    Post in-browser simulator links
    Decent workaround for Expo client app limitations on iOS.

--- ci/post-expo-comment.js
+++ ci/post-expo-comment.js
@@ -4,6 +4,7 @@
  * Post a comment to the active pull request for this build, if any.
  */
 
+const querystring = require('querystring');
 const octokit = require('@octokit/rest')();
 const _ = require('lodash');
 
@@ -15,6 +16,50 @@ function getQrCodeUrl(content) {
   const base = 'api.qrserver.com/v1/create-qr-code';
   const size = '200x200';
   return `https://${base}?size=${size}&data=${content}`;
+}
+
+function appetizeUrl(expoUrl, options = {}) {
+  const base = 'https://appetize.io/app';
+
+  const defaultOptions = {
+    device: 'iphone6s',
+  };
+  const queryParams = {
+    ...defaultOptions,
+
+    screenOnly: false,
+    autoplay: false,
+    embed: true,
+    xdocMsg: true,
+    xDocMsg: true,
+    debug: true,
+    deviceColor: 'black',
+    orientation: 'portrait',
+    launchUrl: expoUrl,
+    params: JSON.stringify({
+      EXKernelLaunchUrlDefaultsKey: expoUrl,
+      EXKernelDisableNuxDefaultsKey: true,
+    }),
+    scale: 75,
+
+    ...options,
+  };
+  const query = querystring.stringify(queryParams);
+
+  const expoPublicKeys = {
+    ios: '8bnmakzrptf1hv9dq7v7bnteem',
+    android: 'xc1w6f1krd589zhp22a0mgftyw',
+  };
+  const { device } = queryParams;
+  let expoPublicKey;
+  if (device.indexOf('iphone') === 0) {
+    expoPublicKey = expoPublicKeys.ios;
+  } else if (device.indexOf('nexus') === 0) {
+    expoPublicKey = expoPublicKeys.android;
+  } else {
+    throw Error(`Unrecognized device type: ${device}`);
+  }
+  return `${base}/${expoPublicKey}?${query}`;
 }
 
 if (!process.env.CIRCLE_PR_NUMBER && !process.env.CIRCLE_PULL_REQUEST) {
@@ -58,11 +103,33 @@ const storybookUrl = storybookUrlWeb.replace('https', 'exp');
 const appQR = getQrCodeUrl(appUrl);
 const storybookQR = getQrCodeUrl(storybookUrl);
 
-const body = `
-Commit ${vars.commit} deployed to Expo ([app](${appUrlWeb}), [storybook](${storybookUrlWeb})).
+const iosOptions = { device: 'iphone6s' };
+const androidOptions = { device: 'nexus5' };
+const urls = {
+  ios: {
+    app: appetizeUrl(appUrl, iosOptions),
+    storybook: appetizeUrl(storybookUrl, iosOptions),
+  },
+  android: {
+    app: appetizeUrl(appUrl, androidOptions),
+    storybook: appetizeUrl(storybookUrl, androidOptions),
+  },
+};
 
-Scan with the [Expo app](https://expo.io/tools#client) (or the Camera app
-on iOS 11) to load this build.
+const expoBlogUrl =
+  'https://blog.expo.io/upcoming-limitations-to-ios-expo-client-8076d01aee1a';
+
+const body = `
+Commit ${vars.commit} deployed to Expo.
+
+Open a simulator in your browser:
+
+| Platform |   |   |
+| -------- | - | - |
+| iOS      | [app](${urls.ios.app})     | [storybook](${urls.ios.storybook}) |
+| Android  | [app](${urls.android.app}) | [storybook](${urls.android.storybook}) |
+
+Scan with the [Expo app](https://expo.io/tools#client) ([Android only](${expoBlogUrl})) to load this build.
 
 | <h3>app</h3>  | <h3>storybook</h3>  |
 | ------------- | ------------------- |
