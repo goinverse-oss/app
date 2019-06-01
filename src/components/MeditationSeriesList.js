0f97d0df6de8aaa59b65a5ed06af1cada5f890a2
Brett Higgins <brett.higgins@gmail.com>
2019-06-01 16:24:38 -0400

    Extract SeriesList component and fix margin mismatch

--- src/components/MeditationSeriesList.js
+++ src/components/MeditationSeriesList.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, StyleSheet, View } from 'react-native';
 
+import SeriesList from './SeriesList';
 import MeditationSeriesTile from './MeditationSeriesTile';
 import MeditationCategory from '../state/models/MeditationCategory';
 
-const styles = StyleSheet.create({
-  list: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start',
-    paddingVertical: 7,
-  },
-});
-
-const MeditationSeriesList = ({ meditationCategories, onPressMeditationCategory }) =>
-  (
-    <ScrollView>
-      <View style={styles.list}>
-        {meditationCategories.map(meditationCategory => (
-          <MeditationSeriesTile
-            key={meditationCategory.title}
-            meditationCategory={meditationCategory}
-            onPress={() => onPressMeditationCategory(meditationCategory)}
-          />
-        ))}
-      </View>
-    </ScrollView>
-  );
+const MeditationSeriesList = ({ meditationCategories, onPressMeditationCategory }) => (
+  <SeriesList>
+    {meditationCategories.map(meditationCategory => (
+      <MeditationSeriesTile
+        key={meditationCategory.title}
+        meditationCategory={meditationCategory}
+        onPress={() => onPressMeditationCategory(meditationCategory)}
+      />
+    ))}
+  </SeriesList>
+);
 
 MeditationSeriesList.propTypes = {
   meditationCategories: PropTypes.arrayOf(
