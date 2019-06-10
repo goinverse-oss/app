df6a97dd98fb40f675aa522f1975e474884ea32b
Brett Higgins <brett.higgins@gmail.com>
2019-06-09 21:39:06 -0400

    Eject to ExpoKit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "theliturgists.app://192.168.7.67:19000";
+
+}
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
   },
   slideTop: {
     justifyContent: 'flex-end',
