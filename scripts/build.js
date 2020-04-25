408f9251c1c50faf6992afa64954c8e79fcf19d6
Brett Higgins <brett.higgins@gmail.com>
2020-04-24 23:47:59 -0400

    Rename ios project again to fix release channel issues

--- scripts/build.js
+++ scripts/build.js
@@ -42,63 +42,10 @@ function build(platform, { uploadBeta }) {
 async function main(argv) {
   const platform = argv._[0];
 
-  let configFile;
-  let config;
-  if (platform === 'ios') {
-    configFile = 'ios/the-liturgists/Supporting/Expo.plist';
-
-    const plistText = await fs.promises.readFile(configFile, 'utf-8');
-    const plistObj = plist.parse(plistText);
-    plistObj.EXUpdatesReleaseChannel = argv.releaseChannel;
-    config = plist.build(plistObj);
-  } else if (platform === 'android') {
-    configFile = 'android/app/src/main/AndroidManifest.xml';
-
-    const manifestText = await fs.promises.readFile(configFile, 'utf-8');
-    const manifestObj = await xml2js.parseStringPromise(manifestText);
-    manifestObj.manifest.application[0]['meta-data'].push(
-      {
-        $: {
-          'android:name': 'expo.modules.updates.EXPO_RELEASE_CHANNEL',
-          'android:value': argv.releaseChannel,
-        },
-      },
-    );
-    config = new xml2js.Builder({ headless: true }).buildObject(manifestObj);
-  } else {
-    throw new Error(`Unknown platform ${platform}`);
-  }
-
-  // 1) Back up existing file
-  const backupConfigFile = `${configFile}.bak`;
-  console.log(`Backing up ${configFile}`);
-  await fs.promises.rename(configFile, backupConfigFile);
-
-  async function restoreConfig() {
-    console.log(`Restoring original ${configFile}`);
-    await fs.promises.rename(backupConfigFile, configFile);
-  }
-
-  // ignore interrupt; build will bail out
-  process.on('SIGINT', () => {});
-
-  try {
-    // 2) Write file with new value for release channel
-    await fs.promises.writeFile(configFile, config);
-
-    // 3) Run build
-    await build(platform, argv);
-  } finally {
-    // 4) Restore old config file
-    await restoreConfig();
-  }
+  await build(platform, argv);
 }
 
 const argv = yargs
-  .option('release-channel', {
-    default: 'default',
-    describe: 'Expo release channel to build into the app',
-  })
   .option('upload-beta', {
     boolean: true,
     describe: 'Build beta release and publish to testing service',
