69da692885569d07d3ecbd969a9124801bfc50eb
Brett Higgins <brett.higgins@gmail.com>
2020-04-30 23:24:08 -0400

    Put Sentry public DSN in code
    It's not a secret, and it was accidentally getting left out of beta builds.

--- ci/deploy-to-expo.sh
+++ ci/deploy-to-expo.sh
@@ -3,7 +3,7 @@
 yarn global add expo-cli
 
 fail=
-for name in EXPO_USERNAME EXPO_PASSWORD CIRCLE_BRANCH SENTRY_AUTH_TOKEN SENTRY_DSN ; do
+for name in EXPO_USERNAME EXPO_PASSWORD CIRCLE_BRANCH SENTRY_AUTH_TOKEN ; do
   eval value=\$$name
   if [[ -z ${value} ]]; then
     echo >&2 "Missing required env variable: ${name}"
@@ -18,8 +18,7 @@ fi
 api_url="https://staging.api.theliturgists.com"
 json -I -f config.json \
   -e "this.apiBaseUrl='${api_url}'" \
-  -e "this.notificationScope='staging'" \
-  -e "this.sentryPublicDSN='${SENTRY_DSN}'"
+  -e "this.notificationScope='staging'"
 
 expo login -u "${EXPO_USERNAME}" -p "${EXPO_PASSWORD}"
 
