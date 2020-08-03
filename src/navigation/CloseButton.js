2e96357b9701a1865bbc8a7ea6ca7c733087c0c4
Brett Higgins <brett.higgins@gmail.com>
2020-08-02 22:57:04 -0400

    Community scren fixes for Android

---
+++ src/navigation/CloseButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { withNavigation } from 'react-navigation';
+
+import Icon from '@expo/vector-icons/MaterialIcons';
+import appPropTypes from '../propTypes';
+
+const styles = StyleSheet.create({
+  closeIcon: {
+    color: '#cccccc',
+    fontSize: 24,
+    paddingHorizontal: 10,
+  },
+});
+
+/**
+ * Button that, when pressed, dismisses the current modal screen.
+ */
+const CloseButton = ({ navigation }) => (
+  <Icon
+    name="close"
+    style={styles.closeIcon}
+    onPress={() => navigation.goBack(null)}
+  />
+);
+
+CloseButton.propTypes = {
+  navigation: appPropTypes.navigation.isRequired,
+};
+
+export default withNavigation(CloseButton);
